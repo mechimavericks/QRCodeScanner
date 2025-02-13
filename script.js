69ffb576035c8e19a22ad7e734392fa7ffe1b658
Santosh Bhandari <santoshvandari1@gmail.com>
2025-02-13 11:05:41 +0545

    Fixed the Color Coding

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
         try {
             const userData = JSON.parse(data);
             document.getElementById("message").textContent = `QR Code Scanned!`;
+            document.getElementById("message").style.color = "green"; // Add green color for success
             document.getElementById("name").value = userData.name || '';
             document.getElementById("email").value = userData.email || '';
             document.getElementById("rollno").value = userData.rollno || '';
         } catch (error) {
             console.log("Qr Scan Error", error);
-
             document.getElementById("message").textContent = "Invalid QR Code";
+            document.getElementById("message").style.color = "red"; // Add red color for error
         }
     }
 
@@ -32,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
             message.style.color = "red";
             submitBtn.disabled = false;
             submitBtn.textContent = "Submit";
-            return
+            return;
         }
 
         fetch(`${serverUrl}?email=${email}`, {
@@ -41,11 +42,13 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 message.innerHTML = data.detail;
+                message.style.color = "green"; // Add green color for success
                 submitBtn.disabled = false;
                 submitBtn.textContent = "Submit";
             }).catch(error => {
-                console.error("Fetch error:", error); // Log the error
-                message.innerHTML = "An error occured";
+                console.error("Fetch error:", error);
+                message.innerHTML = "An error occurred";
+                message.style.color = "red"; // Add red color for error
                 submitBtn.disabled = false;
                 submitBtn.textContent = "Submit";
             });
